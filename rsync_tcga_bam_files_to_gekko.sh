d5a083bc396aa8b840fa5c6eaeb4a140834a9cbb
sufyazi <sufyazi@outlook.com>
2023-09-20 11:28:27 +0800

    add bam file transfer script for gekko

---
+++ rsync_tcga_bam_files_to_gekko.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+################## ONLY RUN THIS SCRIPT ON THE SERVER WHERE THE SOURCE_DIR IS LOCATED ##################
+
+# Check if the correct number of arguments were provided
+if [[ "$#" -ne 3 ]]
+then
+  echo -e "Usage: $0 [--dry-run|--live-run] <filenames_txt_directory> <dataset_id_to_import.txt>"
+  exit 1
+fi
+
+# Set the source directory to traverse
+SOURCE_DIR="/data/shop/accRSXRT5.jmcarter/v1/depo"
+
+# Set the remote server to rsync to
+REMOTE_SERVER="suffi.azizan@gekko.hpc.ntu.edu.sg"
+
+# Set the target path on the remote server
+REMOTE_PATH="/home/suffi.azizan/scratchspace/inputs/fastq/tcga-atac"
+
+# Check if the --dry-run option is provided
+if [[ "$1" == "--dry-run" ]]
+then
+  RUN="$1"
+  echo -e "Running in dry mode\n"
+else
+  RUN="--live-run"
+  echo -e "Running in live mode\n"
+fi
+
+# Set the directory where filename text files are stored
+FILENAME_DIR="$2"
+
+# Process the dataset_id_to_import.txt file
+IMPORT_LIST="$3"
+
+# Loop through the data IDs in the file
+while read -r DATA_ID
+do
+    # Initialize counter
+    COUNTER=0
+
+    # find the text file matching the DATA_ID in its name
+    DATA_FILE=$(find "$FILENAME_DIR" -name "$DATA_ID*" -type f)
+
+    # check if DATA_FILE is empty
+    if [[ -z "$DATA_FILE" ]]; then
+        echo "No file found for $DATA_ID"
+        continue
+    else
+        echo "Found file $DATA_FILE"
+        # map the content of matched file to an array
+        mapfile -t DATA_ARRAY < "$DATA_FILE"
+        echo "Filenames in $DATA_FILE: " "${DATA_ARRAY[@]}"
+    fi
+    
+    echo -e "\nSearching for files in $SOURCE_DIR...\n"
+
+    # Loop through the filenames in the array and search for them in the SOURCE_DIR
+    for FILENAME in "${DATA_ARRAY[@]}"; do
+      BAM_PATH=$(find "$SOURCE_DIR"/ -name "${FILENAME}" -type f)
+      if [[ -z "$BAM_PATH" ]]; then
+        echo "No file found for $FILENAME"
+        continue
+      else
+        ((COUNTER++))
+        echo "Found file $BAM_PATH" "[File number $COUNTER]"
+        FILE_BASENAME=$(basename "${BAM_PATH%%.bam}")
+        BAI_NAME="${FILE_BASENAME}.bai"
+        echo "Finding the corresponding .bai file: ${BAI_NAME}"
+        BAI_PATH=$(find "$SOURCE_DIR"/ -name "${BAI_NAME}" -type f)
+        # Check if the .bai file exists
+        if [[ -n $BAI_PATH ]]; then
+          echo "Found file $BAI_PATH"
+          # Check if the --dry-run option is provided
+          if [[ "$RUN" == "--dry-run" ]]
+          then
+            echo -e "Rsyncing $BAM_PATH and its index file to $REMOTE_SERVER... [DRY-RUN]\n"
+            rsync -aPvhXz --dry-run "$BAM_PATH" "$BAI_PATH" "${REMOTE_SERVER}":"${REMOTE_PATH}"/"$DATA_ID"/
+          else
+            echo -e "Rsyncing $BAM_PATH and its index file to $REMOTE_SERVER... [LIVE]\n"
+            rsync -aPvhXz "$BAM_PATH" "$BAI_PATH" "${REMOTE_SERVER}":"${REMOTE_PATH}"/"$DATA_ID"/
+          fi
+        else
+          echo "No .bai file found for $FILE_BASENAME"
+          # Check if the --dry-run option is provided
+          if [[ "$RUN" == "--dry-run" ]]
+          then
+            echo -e "Rsyncing $BAM_PATH without index file to $REMOTE_SERVER... [DRY-RUN]\n"
+            rsync -aPvhXz --dry-run "$BAM_PATH" "${REMOTE_SERVER}":"${REMOTE_PATH}"/"$DATA_ID"/
+          else
+            echo -e "Rsyncing $BAM_PATH without index file to $REMOTE_SERVER... [LIVE]\n"
+            rsync -aPvhXz "$BAM_PATH" "${REMOTE_SERVER}":"${REMOTE_PATH}"/"$DATA_ID"/
+          fi
+        fi 
+      fi
+    done
+done < "$IMPORT_LIST"
