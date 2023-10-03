796cb785851ee66f21e627f5ce460be35ada539f
sufyazi <sufyazi@outlook.com>
2023-10-03 18:58:30 +0800

    finish with KIRP

--- encd-atac-pl_submitter-v3-ASPIRE.sh
+++ encd-atac-pl_submitter-v3-ASPIRE.sh
@@ -20,7 +20,7 @@ if [[ $# -ne 5 ]]; then
 fi
 
 # Set the number of jobs to submit at a time; this number should be unchanged for now so I will hardcode it
-declare -i MAX_JOBS=17
+declare -i MAX_JOBS=22
 
 # # Set the cluster queue to use (-r: read-only)
 # declare -r CLUSTER_QUEUE="q128"
--- encd-atac-pl_watcher-v3-ASPIRE.sh
+++ encd-atac-pl_watcher-v3-ASPIRE.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 # Clean up module environment and set up job-specific environment
 
 # Set the number of jobs to submit at a time; this number should be unchanged for now so I will hardcode it
-declare -i MAX_JOBS=17
+declare -i MAX_JOBS=22
 
 # Assign the arguments to variables
 analysis_id=$1
